dd36b34d9b300f5cc95dde574a34d77bd50419eb
unknown <leeleegill20@gmail.com>
2019-08-26 22:12:04 -0400

    Removed grab bag

--- NPCs/Mutant.cs
+++ NPCs/Mutant.cs
@@ -1751,14 +1751,6 @@ namespace Fargowiltas.NPCs
             }
         }
 
-        public override void NPCLoot()
-        {
-            if (Main.rand.Next(4) == 0)
-            {
-                Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MutantGrabBag"));
-            }
-        }
-
         //gore
         public override void HitEffect(int hitDirection, double damage)
         {
