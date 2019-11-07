84f2158deeb5172fbea98fe4c3538d98b78524fa
Terry N. Muse <darius.loyhrs@hotmail.com>
2019-11-06 20:14:51 -0500

    ice golem core no longer displays when held

--- Items/Summons/Deviantt/CoreoftheFrostCore.cs
+++ Items/Summons/Deviantt/CoreoftheFrostCore.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -14,6 +15,13 @@ namespace Fargowiltas.Items.Summons.Deviantt
         {
             DisplayName.SetDefault("Core of the Frost Core");
             Tooltip.SetDefault("Summons Ice Golem");
+            Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(6, 4));
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            item.noUseGraphic = true;
         }
     }
 }
