0f50022c90cba75323237d6e5ce6a43e8b2782e8
Tymon <t.simonides@live.nl>
2019-12-13 22:06:20 +0100

    Fuck

--- Items/FargoGlobalItem.cs
+++ Items/FargoGlobalItem.cs
@@ -195,7 +195,6 @@ namespace Fargowiltas.Items
             }
             else if (Array.IndexOf(Thrown, item.type) > -1)
             {
-                Main.NewText(ItemID.GetUniqueKey(item.type).Replace("Terraria ", string.Empty));
                 newType = mod.ItemType(ItemID.GetUniqueKey(item.type).Replace("Terraria ", string.Empty) + "Thrown");
             }
 
