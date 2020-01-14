ed3f3015ce91333407c8370c5b19b5696420c025
Tymon <t.simonides@live.nl>
2020-01-14 16:50:50 +0100

    Skeleton merchant catch fix

--- FargoPlayer.cs
+++ FargoPlayer.cs
@@ -93,7 +93,10 @@ namespace Fargowiltas
 
                             if (Fargowiltas.ModLoaded["FargowiltasSouls"])
                             {
-                                Fargos();
+                                if (player.GetModPlayer<FargowiltasSouls.FargoPlayer>().NinjaEnchant)
+                                {
+                                    player.AddBuff(ModLoader.GetMod("FargowiltasSouls").BuffType("FirstStrike"), 60);
+                                }
                             }
 
                             rodCD = 30;
@@ -202,14 +205,6 @@ namespace Fargowiltas
                 {
                     caughtType = mod.ItemType("ShadowCrate");
                 }
-            }
-        }
-
-        private void Fargos()
-        {
-            if (player.GetModPlayer<FargowiltasSouls.FargoPlayer>().NinjaEnchant)
-            {
-                player.AddBuff(ModLoader.GetMod("FargowiltasSouls").BuffType("FirstStrike"), 60);
             }
         }
     }
