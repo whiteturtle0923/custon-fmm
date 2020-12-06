6c1f66ccb44c4a0e0df1a63c24e9a34b43f8ca92
Terry N. Muse <darius.loyhrs@hotmail.com>
2020-12-06 18:43:37 -0500

    instatrack start

---
+++ Items/Explosives/InstaTrack.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fargowiltas.Items.Explosives
+{
+    public class InstaTrack : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Instatrack");
+            Tooltip.SetDefault("Creates a minecart track across the whole world" +
+                               "\nAlso clears the area right above the tracks" +
+                               "\nDo not use if any important building is nearby");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 10;
+            item.height = 32;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.rare = ItemRarityID.Green;
+            item.UseSound = SoundID.Item1;
+            item.useAnimation = 20;
+            item.useTime = 20;
+            item.value = Item.buyPrice(0, 0, 3);
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.shoot = mod.ProjectileType("InstatrackProj");
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Vector2 mouse = Main.MouseWorld;
+
+            Projectile.NewProjectile(mouse, Vector2.Zero, type, 0, 0, player.whoAmI);
+
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.FossilOre, 20);
+            recipe.AddIngredient(ItemID.Dynamite, 10);
+            recipe.AddIngredient(ItemID.MinecartTrack, 1000);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/Explosives/InstatrackProj.cs
@@ -0,0 +1,80 @@
+﻿using Fargowiltas.Tiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fargowiltas.Projectiles.Explosives
+{
+    public class InstatrackProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Instatrack");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 20;
+            projectile.height = 36;
+            projectile.aiStyle = 16;
+            projectile.friendly = true;
+            projectile.penetrate = -1;
+            projectile.timeLeft = 1;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Vector2 position = projectile.Center;
+            Main.PlaySound(SoundID.Item14, (int)position.X, (int)position.Y);
+
+            if (Main.netMode == NetmodeID.MultiplayerClient)
+            {
+                return;
+            }
+
+            // All the way across
+            for (int x = 1; x <= Main.maxTilesX; x++)
+            {
+                // Six down, last is platforms
+                for (int y = -5; y <= 0; y++)
+                {
+                    int xPosition = x;
+                    int yPosition = (int)(y + position.Y / 16.0f);
+
+                    if (xPosition < 0 || xPosition >= Main.maxTilesX || yPosition < 0 || yPosition >= Main.maxTilesY)
+                        continue;
+
+                    Tile tile = Main.tile[xPosition, yPosition];
+
+                    if (tile == null)
+                    {
+                        continue;
+                    }
+
+                    // Testing for blocks that should not be destroyed
+                    bool noFossil = tile.type == TileID.DesertFossil && !NPC.downedBoss2;
+                    bool noDungeon = (tile.type == TileID.BlueDungeonBrick || tile.type == TileID.GreenDungeonBrick || tile.type == TileID.PinkDungeonBrick) && !NPC.downedBoss3;
+                    bool noHMOre = (tile.type == TileID.Cobalt || tile.type == TileID.Palladium || tile.type == TileID.Mythril || tile.type == TileID.Orichalcum || tile.type == TileID.Adamantite || tile.type == TileID.Titanium) && !NPC.downedMechBossAny;
+                    bool noChloro = tile.type == TileID.Chlorophyte && (!NPC.downedMechBoss1 || !NPC.downedMechBoss2 || !NPC.downedMechBoss3);
+                    bool noLihzahrd = tile.type == TileID.LihzahrdBrick && !NPC.downedGolemBoss;
+
+                    if (noFossil || noDungeon || noHMOre || noChloro || noLihzahrd)
+                    {
+                        continue;
+                    }
+
+                    FargoGlobalTile.ClearEverything(xPosition, yPosition);
+
+                    if (y == 0)
+                    {
+                        // Spawn platforms
+                        WorldGen.PlaceTile(xPosition, yPosition, TileID.MinecartTrack);
+                    }
+
+                    NetMessage.SendTileSquare(-1, xPosition, yPosition, 1);
+                }
+            }
+        }
+    }
+}
