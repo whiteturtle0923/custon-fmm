1c4326d70b34db23e0bbf14dae36c68fbc305e1e
Stevie <xxlennygamerxx@gmail.com>
2021-05-13 21:35:31 -0700

    oopds

--- Fargowiltas.cs
+++ Fargowiltas.cs
@@ -12,6 +12,7 @@ using Fargowiltas.Items.Misc;
 using Fargowiltas.Items.Tiles;
 using Fargowilta;
 using Fargowiltas.Items.CaughtNPCs;
+using Terraria.DataStructures;
 using Terraria.UI;
 
 namespace Fargowiltas
@@ -165,6 +166,9 @@ namespace Fargowiltas
                     censusMod.Call("TownNPCCondition", NPCType("Squirrel"), $"Have a Top Hat Squirrel ([i:{fargoSouls.ItemType("TophatSquirrel")}]) in your inventory");
                 }
             }
+
+            foreach (KeyValuePair<int, int> npc in CaughtNPCItem.CaughtNPCs)
+                Main.RegisterItemAnimation(npc.Key, new DrawAnimationVertical(6, Main.npcFrameCount[npc.Value]));
 
             /*Mod soulsMod = ModLoader.GetMod("FargowiltasSouls");
             if (soulsMod != null)
