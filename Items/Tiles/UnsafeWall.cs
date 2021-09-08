e75977d2fe0a40dc1f879a69db32741e4dfc3883
terrynmuse <darius.loyhrs@hotmail.com>
2021-09-08 12:19:16 -0400

    wall sprites

--- Items/Tiles/UnsafeWall.cs
+++ Items/Tiles/UnsafeWall.cs
@@ -62,66 +62,73 @@ namespace Fargowiltas.Items.Tiles
 
     public class UnsafeBlueSlabWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.BlueBrickWall}";
         public UnsafeBlueSlabWall() : base("Unsafe Blue Slab Wall", WallID.BlueDungeonSlabUnsafe) { }
     }
 
     public class UnsafeBlueTileWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.BlueTiledWall}";
         public UnsafeBlueTileWall() : base("Unsafe Blue Slab Wall", WallID.BlueDungeonTileUnsafe) { }
     }
 
     public class UnsafeBlueBrickWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.BlueBrickWall}";
         public UnsafeBlueBrickWall() : base("Unsafe Blue Brick Wall", WallID.BlueDungeonUnsafe) { }
     }
 
     public class UnsafeGreenSlabWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.GreenBrickWall}";
         public UnsafeGreenSlabWall() : base("Unsafe Green Slab Wall", WallID.GreenDungeonSlabUnsafe) { }
     }
 
     public class UnsafeGreenTileWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.GreenTiledWall}";
         public UnsafeGreenTileWall() : base("Unsafe Green Slab Wall", WallID.GreenDungeonTileUnsafe) { }
     }
 
     public class UnsafeGreenBrickWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.GreenBrickWall}";
         public UnsafeGreenBrickWall() : base("Unsafe Green Brick Wall", WallID.GreenDungeonUnsafe) { }
     }
 
     public class UnsafePinkSlabWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.PinkBrickWall}";
         public UnsafePinkSlabWall() : base("Unsafe Pink Slab Wall", WallID.PinkDungeonSlabUnsafe) { }
     }
 
     public class UnsafePinkTileWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.PinkTiledWall}";
         public UnsafePinkTileWall() : base("Unsafe Pink Slab Wall", WallID.PinkDungeonTileUnsafe) { }
     }
 
     public class UnsafePinkBrickWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.PinkBrickWall}";
         public UnsafePinkBrickWall() : base("Unsafe Pink Brick Wall", WallID.PinkDungeonUnsafe) { }
-    }
-
-    public class UnsafeLihzahrdBrickWall : UnsafeWall
-    {
-        public UnsafeLihzahrdBrickWall() : base("Unsafe Lihzahrd Brick Wall", WallID.LihzahrdBrickUnsafe, ItemID.LihzahrdBrickWall, ItemID.LihzahrdBrick) { }
     }
 
     public class UnsafeSpiderWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.SpiderEcho}";
         public UnsafeSpiderWall() : base("Unsafe Spider Wall", WallID.SpiderUnsafe, ItemID.SpiderEcho) { }
     }
 
     public class UnsafeMarbleWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.MarbleWall}";
         public UnsafeMarbleWall() : base("Unsafe Marble Wall", WallID.MarbleUnsafe, ItemID.MarbleWall, ItemID.Marble) { }
     }
 
     public class UnsafeGraniteWall : UnsafeWall
     {
+        public override string Texture => $"Terraria/Images/Item_{ItemID.GraniteWall}";
         public UnsafeGraniteWall() : base("Unsafe Granite Wall", WallID.GraniteUnsafe, ItemID.GraniteWall, ItemID.Granite) { }
     }
 }
