7bfda8e55e5d159a440f65cd91c6521e52fa98a1
terrynmuse <darius.loyhrs@hotmail.com>
2022-01-24 12:41:42 -0500

    rainbow gift

--- NPCs/Deviantt.cs
+++ NPCs/Deviantt.cs
@@ -163,7 +163,7 @@ namespace Fargowiltas.NPCs
             button = Language.GetTextValue("LegacyInterface.28");
             if (Fargowiltas.ModLoaded["FargowiltasSouls"] && (bool)ModLoader.GetMod("FargowiltasSouls").Call("Masomode"))
             {
-                button2 = (bool)ModLoader.GetMod("FargowiltasSouls").Call("GiftsReceived") ? "Help" : "GIFT"; //"[c/" + Main.DiscoColor.Hex3() + ":Receive Gift]";
+                button2 = (bool)ModLoader.GetMod("FargowiltasSouls").Call("GiftsReceived") ? "Help" : "[c/" + Main.DiscoColor.Hex3() + ":Receive Gift]";
             }
         }
 
