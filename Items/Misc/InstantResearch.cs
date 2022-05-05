f8ce4c22769dfdbfba2bf00d5292649de96bb3e4
terrynmuse <darius.loyhrs@hotmail.com>
2022-05-05 13:35:47 -0400

    added debug item to journey research everything

---
+++ Items/Misc/InstantResearch.cs
@@ -0,0 +1,57 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fargowiltas.Items.Misc
+{
+	public class InstantResearch : ModItem
+	{
+		public override string Texture => "Fargowiltas/Items/Placeholder";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Instant Research");
+			Tooltip.SetDefault("DEBUG ITEM\nResearches everything");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.maxStack = 1;
+			Item.rare = ItemRarityID.Blue;
+			Item.useAnimation = 30;
+			Item.useTime = 30;
+			Item.useStyle = ItemUseStyleID.HoldUp;
+		}
+
+		public override bool? UseItem(Player player)
+		{
+			if (player.itemAnimation > 0 && player.itemTime == 0)
+			{
+				int count = 0;
+
+				for (int i = 0; i < ContentSamples.ItemsByType.Count; i++)
+				{
+					if (CreativeItemSacrificesCatalog.Instance.TryGetSacrificeCountCapToUnlockInfiniteItems(i, out int amountNeeded))
+					{
+						int diff = amountNeeded - player.creativeTracker.ItemSacrifices.GetSacrificeCount(i);
+						if (diff > 0)
+						{
+							player.creativeTracker.ItemSacrifices.RegisterItemSacrifice(i, diff);
+							count++;
+						}
+					}
+				}
+
+				FargoUtils.PrintText($"researched {count} items");
+			}
+
+			return true;
+		}
+	}
+}
