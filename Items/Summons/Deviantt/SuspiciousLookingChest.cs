9d50df24b80c5758093c488d280a9f237d94f607
terrynmuse <darius.loyhrs@hotmail.com>
2022-05-09 21:55:18 -0400

    -Fixed Suspicious Looking Chest not actually summoning Ice Mimic in snow biome as advertised

--- Items/Summons/Deviantt/SuspiciousLookingChest.cs
+++ Items/Summons/Deviantt/SuspiciousLookingChest.cs
@@ -1,10 +1,11 @@
+using Terraria;
 using Terraria.ID;
 
 namespace Fargowiltas.Items.Summons.Deviantt
 {
     public class SuspiciousLookingChest : BaseSummon
     {
-        public override int NPCType => NPCID.Mimic;
+        public override int NPCType => Main.LocalPlayer.ZoneSnow ? NPCID.IceMimic : NPCID.Mimic;
 
         public override string NPCName => "Mimic";
 
@@ -13,7 +14,7 @@ namespace Fargowiltas.Items.Summons.Deviantt
             base.SetStaticDefaults();
             DisplayName.SetDefault("Suspicious Looking Chest");
             Tooltip.SetDefault("Summons Mimic"
-            + "\nSummons Ice Mimic when in underground snow biome");
+            + "\nSummons Ice Mimic when in snow biome");
         }
     }
 }
