4ac90976c527ff7cc1b339b124cd0759f4b2f770
JavyzTaken <jacklukas@outlook.com>
2023-09-28 01:16:22 +0200

    Added ModCalls for Dash Key and Set Bonus Key

--- Fargowiltas.cs
+++ Fargowiltas.cs
@@ -367,6 +367,10 @@ namespace Fargowiltas
                     case "LowRenderProj":
                         ((Projectile)args[1]).GetGlobalProjectile<FargoGlobalProjectile>().lowRender = true;
                         break;
+                    case "DashKeyJustPressed":
+                        return DashKey.JustPressed;
+                    case "SetBonusKeyJustPressed":
+                        return SetBonusKey.JustPressed;
                 }
 
             }
