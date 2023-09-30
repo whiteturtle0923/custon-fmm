539c44a853a0dbf0d49dd8ca98b844831a0a339d
JavyzTaken <jacklukas@outlook.com>
2023-09-30 19:59:59 +0200

    Fixed wikithis support

--- Fargowiltas.cs
+++ Fargowiltas.cs
@@ -228,7 +228,7 @@ namespace Fargowiltas
 
             if (ModLoader.TryGetMod("Wikithis", out Mod wikithis) && !Main.dedServ)
             {
-                wikithis.Call("AddModURL", this, "https://terrariamods.wiki.gg/wiki/Fargo%27s_Mod{}");
+                wikithis.Call("AddModURL", this, "https://terrariamods.wiki.gg/wiki/Fargo%27s_Mod/{}");
 
                 // You can also use call ID for some calls!
                 //wikithis.Call(0, this, "https://examplemod.wiki.gg/wiki/{}");
