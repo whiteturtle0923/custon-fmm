c417cff6c70d3977a6d3140836e8f7b3bec4432c
Furgo <furgos@qq.com>
2023-11-27 22:59:53 +0800

    LumberJack Done

--- NPCs/LumberJack.cs
+++ NPCs/LumberJack.cs
@@ -130,41 +130,23 @@ namespace Fargowiltas.NPCs
 
         public override string GetChat()
         {
-            List<string> dialogue = new List<string>
+            List<string> dialogue = new List<string>();
+            for (int i = 1; i <= 20; i++)
             {
-                "Dynasty wood? Between you and me, that stuff ain't real wood!",
-                "Sure cactus isn't wood, but I can still chop it with me trusty axe.",
-                "You wouldn't by chance have any fantasies about me... right?",
-                "I eat a bowl of woodchips for breakfast... without any milk.",
-                "TIIIIIIIIIMMMBEEEEEEEERRR!",
-                "I'm a lumberjack and I'm okay, I sleep all night and I work all day!",
-                "You won't ever need an axe again with me around.",
-                "I have heard of people cutting trees with fish, who does that?",
-                "You wanna see me work without my shirt on? Maybe in 2030.",
-                "You ever seen the world tree?",
-                "You want what? ...Sorry that's not the kind of wood I'm selling.",
-                "Why don't I sell acorns? ...I replant all the trees I chop, don't you?",
-                "What's the best kind of tree? ... Any if I can chop it.",
-                "Can I axe you a question?",
-                "Might take a nap under a tree later, care to join me?",
-                "I'm an expert in all wood types.",
-                "I'm glad there's more trees to chop here at journey's end.",
-                "Red is one of my favourite colors, right after wood.",
-                "It's always flannel season.",
-                "I'm glad my wood put such a big smile on your face."
-            };
+                dialogue.Add(LumberChat($"Normal{i}"));
+            }
 
             int nurse = NPC.FindFirstNPC(NPCID.Nurse);
             if (nurse >= 0)
             {
-                dialogue.Add($"I always see {Main.npc[nurse].GivenName} looking at my biceps when I'm working. Wonder if she wants some of my wood.");
+                dialogue.Add(LumberChat("Nurse", Main.npc[nurse].GivenName));
             }
 
             Player player = Main.LocalPlayer;
 
             if (player.HeldItem.type == ItemID.LucyTheAxe)
             {
-                dialogue.Add($"Lucy from that universe.. Interesting things await you.");
+                dialogue.Add(LumberChat("LucyTheAxe"));
             }
 
             return Main.rand.Next(dialogue);
@@ -195,14 +177,14 @@ namespace Fargowiltas.NPCs
 
                 if (player.ZoneDesert && !player.ZoneBeach)
                 {
-                    quote = "While I was chopping down a cactus these things leaped at me, why don't you take care of them?";
+                    quote = LumberChat("Desert");
                     itemType = Main.rand.Next(new int[] { ItemID.Scorpion, ItemID.BlackScorpion });
                     player.QuickSpawnItem(player.GetSource_OpenItem(itemType), itemType, 5);
                     player.QuickSpawnItem(player.GetSource_OpenItem(ItemID.Cactus), ItemID.Cactus, 100);
                 }
                 else if (player.ZoneJungle)
                 {
-                    quote = "These mahogany trees are full of life, but a tree only has one purpose: to be chopped. Oh yea these fell out of the last one.";
+                    quote = LumberChat("Jungle");
                     itemType = Main.rand.Next(new int[] { ItemID.Buggy, ItemID.Sluggy, ItemID.Grubby, ItemID.Frog });
                     player.QuickSpawnItem(player.GetSource_OpenItem(itemType), itemType, 5);
                     itemType = Main.rand.Next(new int[] { ItemID.Mango, ItemID.Pineapple });
@@ -211,7 +193,7 @@ namespace Fargowiltas.NPCs
                 }
                 else if (player.ZoneHallow)
                 {
-                    quote = "This place is a bit fanciful for my tastes, but the wood's as choppable as any. Nighttime has these cool bugs though, take a few.";
+                    quote = LumberChat("Hallow");
                     for (int i = 0; i < 5; i++)
                     {
                         itemType = Main.rand.Next(new int[] { ItemID.LightningBug, ItemID.FairyCritterBlue, ItemID.FairyCritterGreen, ItemID.FairyCritterPink });
@@ -226,7 +208,7 @@ namespace Fargowiltas.NPCs
                 }
                 else if (player.ZoneGlowshroom && Main.hardMode)
                 {
-                    quote = "Whatever causes these to glow is beyond me, you're probably gonna eat them anyway so have this while you're at it.";
+                    quote = LumberChat("GlowshroomHM");
                     itemType = Main.rand.Next(new int[] { ItemID.GlowingSnail, ItemID.TruffleWorm });
                     player.QuickSpawnItem(player.GetSource_OpenItem(itemType), itemType, 5);
                     player.QuickSpawnItem(player.GetSource_OpenItem(ItemID.GlowingMushroom), ItemID.GlowingMushroom, 50);
@@ -235,7 +217,7 @@ namespace Fargowiltas.NPCs
                 }
                 else if (player.ZoneCorrupt || player.ZoneCrimson)
                 {
-                    quote = "The trees here are probably the toughest in this branch of reality.. Sorry, just tree puns. I found these for you here.";
+                    quote = LumberChat("Evil");
                     for (int i = 0; i < 5; i++)
                     {
                         itemType = Main.rand.Next(new int[] { ItemID.Elderberry, ItemID.BlackCurrant, ItemID.BloodOrange, ItemID.Rambutan });
@@ -245,14 +227,14 @@ namespace Fargowiltas.NPCs
                 else if (player.ZoneSnow)
                 {
                     //penguin
-                    quote = "This neck of the woods is pretty eh? Here I've got some of my favorite wood for you.";
+                    quote = LumberChat("Snow");
                     itemType = Main.rand.Next(new int[] { ItemID.Cherry, ItemID.Plum });
                     player.QuickSpawnItem(player.GetSource_OpenItem(itemType), itemType, 5);
                     player.QuickSpawnItem(player.GetSource_OpenItem(ItemID.BorealWood), ItemID.BorealWood, 50);
                 }
                 else if (player.ZoneBeach)
                 {
-                    quote = "Even on vacation, I always fit in a little chopping. A couple annoying birds fell out of a palm tree. Take them off my hands.. maybe cook them up?";
+                    quote = LumberChat("Beach");
                     itemType = Main.rand.Next(new int[] { ItemID.Coconut, ItemID.Banana });
                     player.QuickSpawnItem(player.GetSource_OpenItem(itemType), itemType, 5);
                     player.QuickSpawnItem(player.GetSource_OpenItem(ItemID.Seagull), ItemID.Seagull, 5);
@@ -260,7 +242,7 @@ namespace Fargowiltas.NPCs
                 }
                 else if (player.ZoneUnderworldHeight)
                 {
-                    quote = "I looked around here for a while and eventually found some weird, ashy trees. Very fragile. I also found these little guys. Please take them, they're hot to hold.";
+                    quote = LumberChat("Underworld");
                     for (int i = 0; i < 5; i++)
                     {
                         player.QuickSpawnItem(player.GetSource_OpenItem(ItemID.AshWood), ItemID.AshWood, 50);
@@ -273,7 +255,7 @@ namespace Fargowiltas.NPCs
                 {
 					if (Main.rand.NextBool(2))
 					{
-						quote = "I certainly didn't expect to find such wonderful trees down here. Check this out.";
+						quote = LumberChat("DirtRockGem");
 
 						for (int i = 0; i < 5; i++)
 						{
@@ -286,7 +268,7 @@ namespace Fargowiltas.NPCs
 					}
 					else
 					{
-						quote = "Went for a long haul today, but there were only so many of those strange trees to go around. I did find a lot of these, why don't you give some of them a new home?";
+						quote = LumberChat("DirtRockMouse");
 						
 						itemType = ItemID.Mouse;
 						player.QuickSpawnItem(player.GetSource_OpenItem(itemType), itemType, 5);
@@ -299,13 +281,13 @@ namespace Fargowiltas.NPCs
                     {
 						if (Main.WindyEnoughForKiteDrops && Main.rand.NextBool(2)) //ladybug
 						{
-							quote = "Seems like the wind brought a bunch of these out of hiding. Some people say they're good luck. All I know is, the only luck I need is a sharp axe!";
+							quote = LumberChat("CommonDayTimeWindy");
 							itemType = ItemID.LadyBug;
 							player.QuickSpawnItem(player.GetSource_OpenItem(itemType), itemType, 5);
 						}
                         else if (Main.rand.NextBool(3)) //butterfly
                         {
-                            quote = "Back in the day, people used to forge butterflies into powerful gear. We try to forget those days... but here have one.";
+                            quote = LumberChat("CommonDayTimeButterfly");
 							for (int i = 0; i < 5; i++)
                             {
 								itemType = Main.rand.Next(new int[] { ItemID.JuliaButterfly, ItemID.MonarchButterfly, ItemID.PurpleEmperorButterfly, ItemID.RedAdmiralButterfly, ItemID.SulphurButterfly, ItemID.TreeNymphButterfly, ItemID.UlyssesButterfly, ItemID.ZebraSwallowtailButterfly });
@@ -314,12 +296,12 @@ namespace Fargowiltas.NPCs
                         }
                         else if (Main.rand.NextBool(20))
                         {
-                            quote = "I found this, but I'm not a sappy person. You can have it instead.";
+                            quote = LumberChat("CommonDayTimeEucaluptusSap");
                             player.QuickSpawnItem(player.GetSource_OpenItem(ItemID.EucaluptusSap), ItemID.EucaluptusSap);
                         }
                         else
                         {
-                            quote = "These little critters are always falling out of the trees I cut down. Maybe you can find a use for them?";
+                            quote = LumberChat("CommonDayTimeCritter");
 							for (int i = 0; i < 5; i++)
                             {
 								itemType = Main.rand.Next(new int[] { ItemID.Grasshopper, ItemID.Squirrel, ItemID.SquirrelRed, ItemID.Bird, ItemID.BlueJay, ItemID.Cardinal });
@@ -329,7 +311,7 @@ namespace Fargowiltas.NPCs
                     }
                     else
                     {
-                        quote = "Chopping trees at night is always relaxing... well except for the flying eyeballs. Have one of these little guys to keep you company.";
+                        quote = LumberChat("CommonNightTime");
                         player.QuickSpawnItem(player.GetSource_OpenItem(ItemID.Firefly), ItemID.Firefly);
                     }
 
@@ -347,7 +329,7 @@ namespace Fargowiltas.NPCs
             }
             else
             {
-                Main.npcChatText = "I'm resting after a good day of chopping, come back tomorrow and maybe I'll have something else for you.";
+                Main.npcChatText = LumberChat("Rest");
             }
         }
 
@@ -440,6 +422,8 @@ namespace Fargowiltas.NPCs
                 }
             }
         }
+
+        private static string LumberChat(string key, params object[] args) => Language.GetTextValue($"Mods.Fargowiltas.NPCs.LumberJack.Chat.{key}", args);
     }
 
     public class LumberJackProfile : ITownNPCProfile
